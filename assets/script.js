8f9d683ef14a18e906f8045341f043c1eaf01a93
DKK <DhivyaK@ocs-consulting.com>
2023-11-29 18:15:13 +0000

    completed adding JS code

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+
+
+var shapes =["HTML", "CSS", "Git", "JavaScript"];
+ //using for loop to interate through the elements of the array
+ for(var x=0; x < shapes.length;x++)
+ {
+   console.log(shapes[x]);
+ }
+
+//=== is strictly equality operator
+/*
+var topic = "HTML";
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+  } else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (topic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  }*/
+
+ 
+
+ 
